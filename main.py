bccd19389147247085e03dc4e3e6a516fe23a3a6
brettmorrisonA05ANNEX <brettsterm99@gmail.com>
2026-02-10 12:30:37 -0800

    added basic text and arrangement

--- main.py
+++ main.py
@@ -4,50 +4,88 @@ class Cipher(Scene):
     def construct(self):
         # create outer ring cipher
         outer_ring = VGroup()
+        inner_ring = VGroup()
+        cipher = VGroup(outer_ring, inner_ring)
         outer_circle = Circle(radius=3.5, color=WHITE)
         middle_circle = Circle(radius=2.5, color=WHITE)
-        outer_ring.add(outer_circle, middle_circle)
-
-        for i in range(26):
-            angle = i * (360 / 26)
-            line_intersection = Line(middle_circle.point_at_angle(angle * DEGREES), 
-                                     outer_circle.point_at_angle(angle * DEGREES))
-            outer_ring.add(line_intersection)
-
-        outer_letters = VGroup()
-        outer_letter_radius = Circle(radius=3)
-        for i in range(26):
-            angle = 90 - (i * (360 / 26))
-            letter = Text(chr(65 + i), font_size=24)
-            letter.move_to(outer_letter_radius.point_at_angle(angle * DEGREES))
-            letter.rotate((angle - 90) * DEGREES)
-            outer_letters.add(letter)
-        outer_ring.add(outer_letters)
-
-        # create inner ring cipher
-        inner_ring = VGroup()
         inner_circle = Circle(radius=1.5, color=WHITE)
+        outer_ring.add(outer_circle, middle_circle)
         inner_ring.add(inner_circle)
 
         for i in range(26):
             angle = i * (360 / 26)
-            line_intersection = Line(inner_circle.point_at_angle(angle * DEGREES), 
-                                     middle_circle.point_at_angle(angle * DEGREES))
-            inner_ring.add(line_intersection)
 
+            # outer ring intersections
+            outer_line_intersection = Line(middle_circle.point_at_angle(angle * DEGREES), 
+                                     outer_circle.point_at_angle(angle * DEGREES))
+            outer_ring.add(outer_line_intersection)
+
+            # inner ring intersections
+            inner_line_intersection = Line(inner_circle.point_at_angle(angle * DEGREES), 
+                                     middle_circle.point_at_angle(angle * DEGREES))
+            inner_ring.add(inner_line_intersection)
+
+        outer_letters = VGroup()
         inner_letters = VGroup()
+        outer_letter_radius = Circle(radius=3)
         inner_letter_radius = Circle(radius=2)
+
         for i in range(26):
             angle = 90 - (i * (360 / 26))
-            letter = Text(chr(65 + i), font_size=20)
-            letter.move_to(inner_letter_radius.point_at_angle(angle * DEGREES))
-            letter.rotate((angle - 90) * DEGREES)
-            inner_letters.add(letter)
+
+            # outer ring letters
+            outer_letter = Text(chr(65 + i), font_size=24)
+            outer_letter.move_to(outer_letter_radius.point_at_angle(angle * DEGREES))
+            outer_letter.rotate((angle - 90) * DEGREES)
+            outer_letters.add(outer_letter)
+
+            # inner ring letters
+            inner_letter = Text(chr(65 + i), font_size=20)
+            inner_letter.move_to(inner_letter_radius.point_at_angle(angle * DEGREES))
+            inner_letter.rotate((angle - 90) * DEGREES)
+            inner_letters.add(inner_letter)
+
+        outer_ring.add(outer_letters)
         inner_ring.add(inner_letters)
 
         self.play(FadeIn(outer_ring), FadeIn(inner_ring))
+        self.play(cipher.animate.scale(0.8))
+        self.play(cipher.animate.shift(LEFT * 3))
+
+        plaintext = "MESSAGE"
+        key = "KEY"
+
+        self.play(FadeIn(Text("Plaintext: " + plaintext, font_size=32).next_to(cipher, RIGHT * 3).shift(UP)),
+                  FadeIn(Text("Key: " + key, font_size=32).next_to(cipher, RIGHT * 3)),
+                  FadeIn(Text("Ciphertext: ", font_size=32).next_to(cipher, RIGHT * 3).shift(DOWN)))
         self.wait(2)
-        self.play(Rotate(inner_ring, (-3 * (360 / 26)) * DEGREES), run_time=2)
+
+        plaintext_len = len(plaintext)
+        key_len = len(key)
+        plaintext_indices = [ord(char) - 65 for char in plaintext]
+        key_indices = [ord(char) - 65 for char in key]
+
+        # Only rotate the inner ring for the first character of the plaintext
+        self.play(Rotate(inner_ring, key_indices[0] * (360 / 26) * DEGREES, run_time=2))
+
+        for i in range(plaintext_len):
+            curr_key_index = key_indices[i % key_len]
+            next_key_index = key_indices[(i + 1) % key_len]
+
+            dist = next_key_index - curr_key_index
+            shift = dist
+
+            if abs(dist) > 13:
+                if dist > 0:
+                    shift -= 26
+                else:
+                    shift += 26
+
+            #self.play(Rotate(inner_ring, shift * (360 / 26) * DEGREES, run_time=2))
+
+
+
+
 
 
 
